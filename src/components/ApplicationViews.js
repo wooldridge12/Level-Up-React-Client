bc8bdfd6013898db8763050b105ceaf878efefa4
wooldridge12 <wooldridgemikey@gmail.com>
2021-08-03 17:21:26 -0400

    done with chap 7 but may need more work to find title or incorrect references

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,20 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { GameList } from "./game/GameList.js"
+import { GameProvider } from "./game/GameProvider.js"
 
 export const ApplicationViews = () => {
     return <>
         <main style={{
             margin: "5rem 2rem",
+            lineHeight: "1.75rem",
             backgroundColor: "lightgoldenrodyellow"
         }}>
-            Application views
+           <GameProvider>
+               <Route exact path="/">
+                   <GameList />
+               </Route>
+           </GameProvider>
         </main>
     </>
 }
