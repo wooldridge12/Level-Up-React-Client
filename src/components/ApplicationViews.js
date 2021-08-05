17501a932da0f72c8015981200a759264af5fcbb
wooldridge12 <wooldridgemikey@gmail.com>
2021-08-05 10:44:10 -0400

    added background to body

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -9,8 +9,7 @@ export const ApplicationViews = () => {
     return <>
         <main style={{
             margin: "5rem 2rem",
-            lineHeight: "1.75rem",
-            backgroundColor: "lightgoldenrodyellow"
+            lineHeight: "1.75rem"
         }}>
            <GameProvider>
                <Route exact path="/">
